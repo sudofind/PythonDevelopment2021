0571ef3efae13a277cd17ba238cec23fefccc98a
Dmitriy Konovalchuk <02.03.1998@list.ru>
2021-03-13 13:30:46 +0300

    Delete global variable 'digits'

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 
-digits = [(i//4, i%4) for i in range(16)]
 
 class Application(tk.Frame):
+    #digits = list()
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
+        self.digits = [(i//4, i%4) for i in range(16)]
         self.createWidgets()
+
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
@@ -15,10 +17,9 @@ class Application(tk.Frame):
         self.newButton.grid(row=0, column=0)
 
         self.digitButtons = []
-        for i, (y, x) in enumerate(digits[:-1]):
-            self.digitButtons.append(tk.Button(self, text=str(i), command=self.check_and_update_digits))
+        for i, (y, x) in enumerate(self.digits[:-1]):
+            self.digitButtons.append(tk.Button(self, text=str(i+1), command=self.check_and_update_digits))
             self.digitButtons[-1].grid(row=y+1, column=x)
-
 
     def set_random(self):
         pass
