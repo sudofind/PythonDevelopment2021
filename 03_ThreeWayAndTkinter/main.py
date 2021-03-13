f1a02005c785e0e41949ae9436083fa1f21ba6c6
Dmitriy Konovalchuk <02.03.1998@list.ru>
2021-03-13 13:18:05 +0300

    Draw buttons with digits

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+digits = [(i//4, i%4) for i in range(16)]
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -8,7 +10,21 @@ class Application(tk.Frame):
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.grid()
+        self.quitButton.grid(row=0, column=1)
+        self.newButton = tk.Button(self, text='New', command=self.set_random)
+        self.newButton.grid(row=0, column=0)
+
+        self.digitButtons = []
+        for i, (y, x) in enumerate(digits[:-1]):
+            self.digitButtons.append(tk.Button(self, text=str(i), command=self.check_and_update_digits))
+            self.digitButtons[-1].grid(row=y+1, column=x)
+
+
+    def set_random(self):
+        pass
+
+    def check_and_update_digits(self):
+        pass
 
 app = Application()
 app.master.title('Sample application')
